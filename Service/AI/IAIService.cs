64a6e456d174e5643c223e7efabf60f2b5f16241
km7624 <51872595+km7624@users.noreply.github.com>
2025-10-30 18:44:06 +0900

    Update IAIService.cs

--- Service/AI/IAIService.cs
+++ Service/AI/IAIService.cs
@@ -4,7 +4,7 @@
     {
         Task<string> GenerateStoryText(string prompt);
         Task<string> EnrichStory(string baseStory);
-        Task<byte[]> GenerateAudio(string text);
+        Task<byte[]> GenerateAudio(string text, IFormFile audioFile);        //10-30 임강묵 수정
         Task<string> TranslateText(string text, string targetLanguage = "en");
         Task<string> CallCallbackHook(string data);
     }
